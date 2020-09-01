6ad5a483db77cc9071c6bef8dbbac73fd07dbd79
Harshal Ahire <ahireharshal@gmail.com>
2020-09-01 00:58:21 -0400

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+region = "us-east-1"
+}
+resource "aws_instance" "claim_dev_server" {
+ami = "ami-02354e95b39ca8dec"
+instance_type = "t2.micro"
+tags = {
+Name = "claim_dev_server"
+}
+}
